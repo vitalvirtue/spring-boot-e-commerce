d6f65678c6a0f07efe63de61b55ba68e8e09a1ff
vitalvirtue <hyterdem@hotmail.com>
2024-05-29 15:55:15 +0300

    product

--- src/main/java/dev/java/project/Controllers/ProductController.java
+++ src/main/java/dev/java/project/Controllers/ProductController.java
@@ -1,28 +1,18 @@
-package dev.java.project.controllers;
+package dev.java.project.Controllers;
 
-import dev.java.project.dto.UserDTO;
 import dev.java.project.model.Product;
-import dev.java.project.model.User;
 import dev.java.project.services.ProductService;
-import dev.java.project.services.UserService;
 import lombok.AllArgsConstructor;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
+
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -38,6 +28,5 @@ public class ProductController {
         
 		return ResponseEntity.ok(productService.getProduct(id));
 	}
-
 
 }
